5df3e06831cc34ebc92cb5324aca4248472759de
Chaitanya Purkar <158091057+chaitanya1013@users.noreply.github.com>
2025-06-30 01:05:19 +0530

    Add files via upload

---
+++ que_1.cpp
@@ -0,0 +1,20 @@
+// Cricket World Cup Qualifier
+// The cricket World Cup has started in Chefland. There are many teams participating in the group stage matches. Any team that scores 12
+// 12 or more points in the group stage matches qualifies for the next stage.
+// You know the score that a particular team has scored in the group stage matches. Determine if the team has qualified for the next stage or not.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	if(n>=12) {
+	    cout<<"Yes";
+	}
+	else {
+	    cout<<"No";
+	}
+	
+}
---
+++ que_2.cpp
@@ -0,0 +1,21 @@
+// Lucky Seven
+// Chef considers the number 7
+// 7 lucky. As a result, he believes that the 7
+// 7-th letter he sees on a day is his lucky letter of the day.
+
+// You are given a string S
+// S of length 10
+// 10, denoting the first 10
+// 10 letters Chef saw today.
+// What is Chef's lucky letter?
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    char Arr[10];
+    cin >> Arr;
+    cout << Arr[6];
+    
+}
