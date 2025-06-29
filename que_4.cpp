1d8c0953a0d094140f73be9a9494506d4ffef901
Chaitanya Purkar <158091057+chaitanya1013@users.noreply.github.com>
2025-06-30 01:22:51 +0530

    Add files via upload

---
+++ que_4.cpp
@@ -0,0 +1,17 @@
+// Double Rent
+// Chefina decided to move into Chef's apartment.
+// Chef was initially paying a rent of 
+// X rupees. Since Chefina is moving in, the owner decided to double the rent.
+// Find the final rent Chef needs to pay
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int a;
+    cin >> a;
+    cout << 2 * a;
+    return 0;
+}
