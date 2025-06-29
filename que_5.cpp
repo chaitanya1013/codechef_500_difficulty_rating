5b696ede7c4bef6506ad980c78d1b33f322af3b8
Chaitanya Purkar <158091057+chaitanya1013@users.noreply.github.com>
2025-06-30 01:31:01 +0530

    Add files via upload

---
+++ que_5.cpp
@@ -0,0 +1,18 @@
+// Saving Taxes
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y;
+	cin>>t;
+	
+	while(t--) {
+	    cin>>x>>y;
+        cout<<x-y<<endl;
+    }
+    return 0;
+}
