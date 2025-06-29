559d3690817ef136f31e41487c3c312cba6889b9
Chaitanya Purkar <158091057+chaitanya1013@users.noreply.github.com>
2025-06-30 01:18:45 +0530

    Add files via upload

---
+++ que_3.cpp
@@ -0,0 +1,19 @@
+// Clear Day
+// Chef classifies a day to be either rainy, cloudy, or clear.
+// In a particular week, Chef finds 
+// X days to be rainy and 
+// Y days to be cloudy.
+// Find the number of clear days in the week.
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here.
+	int a, b;
+	cin >> a >> b;
+	cout << 7 - (a + b); 
+
+}
